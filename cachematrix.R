32588039dc5f1a411d7cbdc0f269306c0948847a
TyroSofaWarrior <kakhmetyanov@nes.ru>
2020-08-09 18:17:43 +0300

    assigment was completed

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## I wrote this code in hope, that it would be able to found inverse matrix and save it in the cache for avoiding repeated calculations
+## Mainly there aren't many differences between function for vectors/mean from the example and required results
 
-## Write a short comment describing this function
-
+##the first step is creating a list containing value and answer of inverse function for matrix, so later we can use it again
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set_value <-function(value) {
+        x <<- value
+        m <<- NULL
+    }
+    get_value <- function() {
+        x
+    }
+    set_answer <- function(answer) {
+        m <<- answer
+    }
+    get_answer <- function(){
+        m
+    }
+    list (set_value = set_value, get_value = get_value, 
+          set_answer = set_answer, get_answer = get_answer)
 }
 
 
-## Write a short comment describing this function
+## then we should use results of caching and ask for previous requests
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ##ask for ready answer in cache
+    m <- x$get_answer()
+    ##solve the task firstly and caching result
+    if (is.null(m)){
+        m <- solve(x$get_value())
+        x$set_answer(m)
+    }
+    ##return inverted matrix
+    m
 }
